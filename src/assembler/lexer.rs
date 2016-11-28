c73086fb271b7555f502aa8b7fa63b124ba1de7f
Simon Whitehead <chemnova@gmail.com>
2016-11-28 14:13:59 +1100

    Start lexer tokenization

--- src/assembler/lexer.rs
+++ src/assembler/lexer.rs
@@ -4,6 +4,7 @@
 use std;
 use std::fs::File;
 use std::io::{self, Read};
+use std::iter::Peekable;
 
 use assembler::token::Token;
 use ::opcodes::OpCode;
@@ -40,28 +41,73 @@ impl Lexer {
         let mut result = Vec::new();
 
         for line in source.lines() {
-            let mut tok = String::new();
+            if line.trim().len() == 0 {
+                continue;
+            }
             let mut tokens = Vec::new();
-            for c in line.chars() {
-                if c != ';' && c != ':' && !c.is_whitespace() {
-                    tok.push(c);
+            let mut idx = 0;
+            let line = &line[..].as_bytes();
+            while idx < line.len() {
+                let c = line[idx] as char;
+                if c.is_alphanumeric() {
+                    tokens.push(Self::consume_alphanumeric(&mut idx, line));
                 } else {
-                    // If the token is not empty, lets classify it and
-                    // put it in the tokens Vector
+                    idx += 1;
                 }
             }
 
-            result.append(&mut tokens);
+            result.push(tokens);
         }
 
         result
+    }
+
+    fn consume_alphanumeric(idx: &mut usize, line: &[u8]) -> Token {
+        let mut tok = String::new();
+
+        while *idx < line.len() {
+            let c = line[*idx] as char;
+            if c.is_whitespace() || c == ';' || c == ':' || c == '\n' {
+                return Self::classify(&tok);
+            } else {
+                tok.push(c);
+            }
+
+            *idx += 1;
+        }
+
+        Token::Unknown(tok)
+    }
+
+    fn classify(input: &str) -> Token {
+        if let Some(opcode) = OpCode::from_mnemonic(input) {
+            Token::OpCode(opcode.mnemonic.into())
+        } else {
+            Token::Unknown(input.into())
+        }
     }
 
     fn create_token(input: &String) -> Token {
         if let Some(opcode) = OpCode::from_mnemonic(&input[..]) {
             Token::OpCode(opcode.mnemonic.into())
         } else {
+
             Token::Unknown(input.clone())
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ::assembler::token::Token;
+
+    #[test]
+    fn can_classify_simple_labels_and_opcodes() {
+        let tokens = Lexer::lex_string("
+            LDA #$20 
+        ");
+
+        assert_eq!(&[Token::OpCode("LDA".into()), Token::Unknown("#$20".into())], &tokens[0][..]);
     }
 }
--- src/assembler/token.rs
+++ src/assembler/token.rs
@@ -1,5 +1,6 @@
 use opcodes::OpCode;
 
+#[derive(Eq, PartialEq, Debug)]
 pub enum Token {
     Unknown(String),
     Comment(String),
