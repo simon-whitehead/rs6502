b7263169bc9b70ff4fdf6dbe2870d79e053f474f
Simon Whitehead <chemnova@gmail.com>
2016-12-08 01:09:17 +1100

    Add basic Assembler functionality and test

--- src/assembler/assembler.rs
+++ src/assembler/assembler.rs
@@ -4,7 +4,7 @@ use std::path::Path;
 use ::opcodes::{AddressingMode, OpCode};
 use assembler::lexer::{Lexer, LexerError};
 use assembler::parser::{Parser, ParserError};
-use assembler::token::LexerToken;
+use assembler::token::{LexerToken, ParserToken};
 
 #[derive(Debug, PartialEq)]
 pub enum Symbol {
@@ -12,6 +12,7 @@ pub enum Symbol {
     Constant(LexerToken), // The constant value
 }
 
+#[derive(Debug)]
 pub struct AssemblerError {
     message: String,
 }
@@ -44,7 +45,7 @@ impl Assembler {
         let mut lexer = Lexer::new();
         let tokens = lexer.lex_string(code)?;
         let mut parser = Parser::new();
-        let tokens = Vec::new(); // TODO: Fix.
+        let tokens = parser.parse(tokens)?;
 
         Ok(self.assemble(tokens))
     }
@@ -60,8 +61,34 @@ impl Assembler {
         Ok(self.assemble(tokens))
     }
 
-    fn assemble(&mut self, tokens: Vec<Vec<LexerToken>>) -> Vec<u8> {
+    fn assemble(&mut self, tokens: Vec<ParserToken>) -> Vec<u8> {
         // First, index the labels so we have addresses for them
-        Vec::new()
+        let mut result = Vec::new();
+
+        for token in tokens {
+            if let ParserToken::OpCode(opcode) = token {
+                result.push(opcode.code);
+            } else if let ParserToken::RawByte(byte) = token {
+                result.push(byte);
+            }
+        }
+
+        result
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn can_assemble_basic_code() {
+        let mut assembler = Assembler::new();
+        let bytes = assembler.assemble_string("
+            LDA $4400
+        ")
+            .unwrap();
+
+        assert_eq!(&[0xAD, 0x00, 0x44], &bytes[..]);
     }
 }
