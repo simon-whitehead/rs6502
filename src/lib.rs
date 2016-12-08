8ca07ea6944c9ac4533825a865fb4f97558e81fd
Simon Whitehead <chemnova@gmail.com>
2016-12-08 22:11:46 +1100

    Add end-to-end test
    Added an end-to-end test that tests assembling a block of 6502 Assembly
    code and disassembles it into a recognisable string.

--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,7 @@ mod disassembler;
 mod opcodes;
 mod registers;
 
+pub use assembler::Assembler;
 pub use cpu::Cpu;
 pub use disassembler::Disassembler;
 pub use opcodes::OpCode;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {}
-}
